bd44325e9183c238da8475fc424ee338522a2f09
Derek Yu <derekyu624@hotmail.com>
2020-09-18 21:41:03 -0400

    Configure basic CMake project

---
+++ build/CMakeFiles/Warzone.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Warzone.dir/main.cpp.obj"
+  "Warzone.exe"
+  "Warzone.exe.manifest"
+  "Warzone.pdb"
+  "libWarzone.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Warzone.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/derek/Documents/Projects/COMP 345 Project/Warzone
+# Build directory: C:/Users/derek/Documents/Projects/COMP 345 Project/Warzone/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
